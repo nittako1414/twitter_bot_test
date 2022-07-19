47d87f499d7bba86645ddb1d5b7cf997521a7b80
nittako1414 <88404807+nittako1414@users.noreply.github.com>
2022-07-19 19:20:16 +0900

    Update holidays.py

--- holidays.py
+++ holidays.py
@@ -1,16 +1,6 @@
 import datetime
-from bs4 import BeautifulSoup
-import requests, json, os
+import requests, json
 import tweepy
-from dotenv import find_dotenv, load_dotenv
-
-
-# .env読み込み
-# load_dotenv()
-# api_key = os.getenv('api_key')
-# api_key_secret = os.getenv('api_key_secret')
-# access_token = os.getenv('access_token')
-# access_token_secret = os.getenv('access_token_secret')
 
 api_key = "rkq7sajRcLyN1axBEEi12jTgh"
 api_key_secret = "un1hEVCysTU4WSjbdwkRW7qGKb4d73PGSItnC9vAyCNGMnWgqA"
@@ -57,30 +47,7 @@ holiday_text = '次の祝日（' + near_holiday_value + '）までは' + diff_da
 # api.update_status(text)
 
 
-# 阿部寛
-# 阿部 寛さんのホームページのURL
-url = 'http://abehiroshi.la.coocan.jp/'
-# iframeで別HTMLを読んでるみたいなのでページのURL
-page_url = 'top.htm'
-
-# ページのデータを取得
-response = requests.get(url + page_url)
-html = response.content
-soup = BeautifulSoup(html, "html.parser")
-# HTMLデータから画像を探す
-img_src = soup.find("img").get("src")
-
-# 画像にアクセス
-resp = requests.get(url + img_src)
-# 404なら知らせる
-if resp.status_code == 404:
-    abe_text = '阿部 寛さんの画像リンクが切れています。'
-else:
-    abe_text = '画像は今日も問題ないです。'
-
 # holiday_textの投稿
-text = "@reentrant1732 今日の巡回結果だよ！\n \n" + holiday_text + '\n' + abe_text
+text = "@reentrant1732 今日の巡回結果だよ！\n \n" + holiday_text + '\n'
 api.update_status(text)
-
-
 
